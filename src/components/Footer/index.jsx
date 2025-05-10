f91080336c9f8c247349781df1430ae03905d0e7
Naslu kk <naslukk@gmail.com>
2025-05-10 14:13:08 +0530

    [fix]: the mobile view fixed

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -11,38 +11,40 @@ import Logo from "../../assets/hoomans-logo.webp";
 
 export default function Footer() {
   return (
-    <div className={style.footerContainer}>
-      <div className={style.footerL}>
-        <a href="">
-          <img src={Logo} alt="logo" />
-        </a>
+    <footer>
+      <div className={style.footerContainer}>
+        <div className={style.footerL}>
+          <a href="">
+            <img src={Logo} alt="logo" />
+          </a>
+        </div>
+        <div className={style.footerR}>
+          <a href="">
+            <FontAwesomeIcon
+              icon={faInstagram}
+              style={{ color: "rgb(164, 164, 164)", fontSize: "20px" }}
+            />
+          </a>
+          <a href="">
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              style={{ color: "rgb(164, 164, 164)", fontSize: "20px" }}
+            />
+          </a>
+          <a href="">
+            <FontAwesomeIcon
+              icon={faXTwitter}
+              style={{ color: "rgb(164, 164, 164)", fontSize: "20px" }}
+            />
+          </a>
+          <a href="">
+            <FontAwesomeIcon
+              icon={faTelegramPlane}
+              style={{ color: "rgb(164, 164, 164)", fontSize: "20px" }}
+            />
+          </a>
+        </div>
       </div>
-      <div className={style.footerR}>
-        <a href="">
-          <FontAwesomeIcon
-            icon={faInstagram}
-            style={{ color: "rgb(164, 164, 164)", fontSize: "20px" }}
-          />
-        </a>
-        <a href="">
-          <FontAwesomeIcon
-            icon={faLinkedin}
-            style={{ color: "rgb(164, 164, 164)", fontSize: "20px" }}
-          />
-        </a>
-        <a href="">
-          <FontAwesomeIcon
-            icon={faXTwitter}
-            style={{ color: "rgb(164, 164, 164)", fontSize: "20px" }}
-          />
-        </a>
-        <a href="">
-          <FontAwesomeIcon
-            icon={faTelegramPlane}
-            style={{ color: "rgb(164, 164, 164)", fontSize: "20px" }}
-          />
-        </a>
-      </div>
-    </div>
+    </footer>
   );
 }
